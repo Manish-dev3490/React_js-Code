4ef6d0a403c2d3e36db2e5342fb50a4084f766ac
manish-dev3490 <kmanish87064@gmail.com>
2024-04-08 13:22:52 +0530

    Header is now done with Login Logout Funcyionalty

--- Swiggy-App/src/components/HeaderComponent.jsx
+++ Swiggy-App/src/components/HeaderComponent.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 // I am creating Header  Component for my website  component is anything you can see in application is a component functional based component is just normal javascript function which rreturn the peice of jsx code
 
+
+// I have to put login and logout functionality in my header which can be done through hooks because they have such special powers
+
  export const HeaderComponent=()=>{
+
+    const [LoginStatus,SetLoginStatus]=useState("Login");
+    let status="Login";
     return (
         <div className="Header">
             <h2>Food Paradise</h2>
@@ -14,6 +21,9 @@
                     <li>About</li>
                     <li>Contact</li>
                     <li>Help</li>
+                    <button className="log-or-not" onClick={()=>{
+                        LoginStatus==="Login"?SetLoginStatus("Logout"):SetLoginStatus("Login");
+                    }}>{LoginStatus}</button>
                 </ul>
             </div>
         </div>
