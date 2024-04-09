f7a6134b02534e15465c7e1bef10b300cfca99de
manish-dev3490 <kmanish87064@gmail.com>
2024-04-09 14:21:02 +0530

    Header Search Filter Hero Section Functionality Done

--- Swiggy-App/src/components/BodyComponent.jsx
+++ Swiggy-App/src/components/BodyComponent.jsx
@@ -3,45 +3,74 @@ import { useEffect, useState } from "react";
 // In body component we have to fetch restaurents card with their live information which we can do through the swiggy orignal production api
 
 export const BodyComponent = () => {
-    const [dataList, SetDataList] = useState([]);
+  const [dataList, SetDataList] = useState([]);
+  const [InputSearch, SetInputsearch] = useState("");
+  const [FilterList, SetFilterList] = useState([]);
 
-    const Fetchdata = async () => {
-        try {
-            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const Fetchdata = async () => {
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
 
-            const json = await data.json();
-            SetDataList(
-                json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
-            );
-        } catch (error) {
-            console.error("something error", error);
-        }
-    };
+      const json = await data.json();
+      SetDataList(
+        json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+      );
+      SetFilterList(
+        json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+      );
+    } catch (error) {
+      console.error("something error", error);
+    }
+  };
 
-    useEffect(() => {
-        Fetchdata();
-    }, []);
+  useEffect(() => {
+    Fetchdata();
+  }, []);
 
-    console.log(dataList);
-    return (
-        
-        <div className="Hero-section">
+  console.log(dataList);
+  return (
+    <div className="Hero-section">
+      <div className="top-rated-filter">
+        <div className="search-box">
+          <input
+            type="text"
+            value={InputSearch}
+            onChange={(e) => {
+              SetInputsearch(e.target.value);
+            }}
+          />
+          <button className="search-btn" onClick={() => {
+           const FilteredList= dataList.filter((check)=>{
+                return check.info.name.toLowerCase().includes(InputSearch.toLowerCase());
+            })
 
-            <div className="top-rated-filter">
-                <button className="top-restaurents-card" onClick={()=>{
-                   const filterResta= dataList.filter((check)=>{
-                        return check.info.avgRating < 4;
-                    })
-                    SetDataList(filterResta);
-                }}>Top Rated Restaurents</button>
-            </div>
-
-            <div className="Resta-list">
-                {
-                    dataList.map((card, index) => { return <Restacard key={index} resData={card.info} /> })
-
-                }
-            </div>
+            SetFilterList(FilteredList);
+          }}>
+            Search
+          </button>
         </div>
-    );
+
+
+        <button
+          className="top-restaurents-card"
+          onClick={() => {
+            const filterResta = dataList.filter((check) => {
+              return check.info.avgRating < 4;
+            });
+            SetDataList(filterResta);
+          }}
+        >
+          Top Rated Restaurents
+        </button>
+      </div>
+
+      <div className="Resta-list">
+        {FilterList.map((card, index) => {
+          return <Restacard key={index} resData={card.info} />;
+        })}
+      </div>
+    </div>
+  );
 };
--- Swiggy-App/src/components/HeaderComponent.jsx
+++ Swiggy-App/src/components/HeaderComponent.jsx
@@ -1,21 +1,16 @@
 import { useState } from "react"
 
-
-
 // I am creating Header  Component for my website  component is anything you can see in application is a component functional based component is just normal javascript function which rreturn the peice of jsx code
-
 export const HeaderComponent = () => {
     // I have to put login and logout functionality in my header which can be done through hooks because they have such special powers
 
     const importantHook = useState("Login");
     const [LoginStatus, SetLoginStatus] = importantHook;
+    
     return (
         <div className="Header">
             <h2>Food Paradise</h2>
-            <div className="search-box">
-                <input type="text" />
-                <button className="search-btn">Search</button>
-            </div>
+            
             <div className="navigation-bar">
                 <ul>
                     <li>Home</li>
