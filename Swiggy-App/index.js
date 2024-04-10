1fc7de6390599311c9fa84e9c5302cfae97fcb49
manish-dev3490 <kmanish87064@gmail.com>
2024-04-10 11:44:02 +0530

    did some correction

--- Swiggy-App/index.js
+++ Swiggy-App/index.js
@@ -7,7 +7,7 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import { AboutPage } from "./src/components/About";
 import { HelpPage } from "./src/components/Help";
 import { Error } from "./src/components/Error";
-import { ContactPage } from "./src/components/contact";
+import { ContactPage } from "./src/components/Contact";
 // I am making a whole Application component in which my header hero section and footer will come
 const ApplicationMainDiv = () => {
     return (
