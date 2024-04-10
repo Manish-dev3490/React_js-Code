1d33da10b841a0890965bd09c899573b61b00734
manish-dev3490 <kmanish87064@gmail.com>
2024-04-10 11:59:03 +0530

    Restaurent detail component is done

--- Swiggy-App/index.js
+++ Swiggy-App/index.js
@@ -8,6 +8,7 @@ import { AboutPage } from "./src/components/About";
 import { HelpPage } from "./src/components/Help";
 import { Error } from "./src/components/Error";
 import { ContactPage } from "./src/components/Contact";
+import { RestaurentDetailPage } from "./src/components/RestaurentDetail";
 // I am making a whole Application component in which my header hero section and footer will come
 const ApplicationMainDiv = () => {
     return (
@@ -39,6 +40,10 @@ const approuter = createBrowserRouter([
             {
                 path: "/contact",
                 element: <ContactPage></ContactPage>
+            },
+            {
+                path:"/restaurent/:resId",
+                element:<RestaurentDetailPage></RestaurentDetailPage>
             }
         ],
         errorElement: <Error />
---
+++ Swiggy-App/src/components/RestaurentDetail.jsx
@@ -0,0 +1,11 @@
+// In this component i will make a detail page of restaurents by  there id and this  concept is known as dynamically routing a diffrent url components will inside my url
+
+ export const RestaurentDetailPage=()=>{
+    return (
+        <div Restaurent-detail-page>
+            <h2>Name of the Restaurent</h2>
+            <img src="" alt="" srcset="" />
+            <h2>What food it offers</h2>
+        </div>
+    )
+}
