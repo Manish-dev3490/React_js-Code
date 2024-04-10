3ca6553c065e7ede5702d668c4b6836db03c4f6e
manish-dev3490 <kmanish87064@gmail.com>
2024-04-10 11:08:26 +0530

    Routing, Linking, Shimmer UI,SearchFeauture ,Login and Logout Feauture and Filters are implemented till now

--- Swiggy-App/src/components/Contact.jsx
+++ Swiggy-App/src/components/Contact.jsx
@@ -3,7 +3,7 @@
 export const ContactPage=()=>{
     return (
         <>
-        <h2>This is the About Page</h2>
+        <h2>This is the Contact Page</h2>
         <h3>This is namastey Javascript series</h3>
         </>
     )
