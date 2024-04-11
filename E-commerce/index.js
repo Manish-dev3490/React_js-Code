ecfe0c5eec17431839661197b34e22aab1735a9d
manish-dev3490 <kmanish87064@gmail.com>
2024-04-11 08:33:19 +0530

    All files are created now we are developing application

---
+++ E-commerce/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const rootOfApplication=ReactDOM.createRoot(document.querySelector(".container"));
+console.log(rootOfApplication);
