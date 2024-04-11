0cd00af2b0c8f8bcb0df8708f01c82933dd4b733
manish-dev3490 <kmanish87064@gmail.com>
2024-04-11 08:46:19 +0530

    Structure of header is done

--- E-commerce/index.js
+++ E-commerce/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+import { HeaderComponent } from "./src/components/HeaderComponent";
 
+
+
+// Let me create a full app component 
+const FullApplicationArea=()=>{
+    return (
+        <>
+        <HeaderComponent/>
+        </>
+    )
+}
+// This is the main root of the aplication where we have to render our all components
 const rootOfApplication=ReactDOM.createRoot(document.querySelector(".container"));
-console.log(rootOfApplication);
+rootOfApplication.render(<FullApplicationArea/>);
---
+++ E-commerce/src/components/HeaderComponent.jsx
@@ -0,0 +1,31 @@
+import 'remixicon/fonts/remixicon.css'
+// In this i am going to ccreate Header component
+
+ export const HeaderComponent=()=>{
+    return (
+        <div className="Header">
+            <div className="logo-section">
+                <h3>Apna <span>Bazaar</span></h3>
+            </div>
+
+            <div className="navigation-section">
+                <ul>
+                    <li>Home</li>
+                    <li>All Products</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Help</li>
+                </ul>
+            </div>
+
+            <div className="login-cart-darkmode">
+                <div className="light-darkmode">
+                <i className="ri-sun-fill"></i>
+                <i className="ri-moon-fill"></i>
+                </div>
+                <div className="login-logout"></div>
+                <div className="cart"><i className="ri-shopping-cart-line"></i></div>
+            </div>
+        </div>
+    )
+}
