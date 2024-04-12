79feafd60a414ebb5b983647047d0e752c5b011c
manish-dev3490 <kmanish87064@gmail.com>
2024-04-12 17:38:03 +0530

    Life cycle method are implemented

--- Swiggy-App/index.js
+++ Swiggy-App/index.js
@@ -45,7 +45,7 @@ const approuter = createBrowserRouter([
             },
             {
                 path:"/restaurent/:resId",
-                element:<RestaData/>
+                element:<RestaurentDetailPage/>
             }
         ],
         errorElement: <Error />
