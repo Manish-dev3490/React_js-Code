e472be30c3512b1a86af9eded39136005c91176a
manish-dev3490 <kmanish87064@gmail.com>
2024-04-15 11:23:24 +0530

    Server started

---
+++ ExploreMart_App/src/index.js
@@ -0,0 +1,10 @@
+// This is our main application file 
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+// getting root from our index.html
+const root=ReactDOM.createRoot(document.querySelector(".root"));
+root.render(<>
+Hello To the react 
+</>)
