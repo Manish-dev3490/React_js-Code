cd1739a59b09f0c9ea07b4978b9bbf4ed210cd74
manish-dev3490 <kmanish87064@gmail.com>
2024-04-15 13:19:56 +0530

    making product display section in hero section

---
+++ ExploreMart_App/src/components/HeroSection.js
@@ -0,0 +1,11 @@
+// In this file i will make a hero section of the application
+
+const HeroSection=()=>{
+    return (
+        <>
+        <div className="hero-section">
+            
+        </div>
+        </>
+    )
+}
