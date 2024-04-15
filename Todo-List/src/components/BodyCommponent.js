376420fa418003897ffd231264d983df6bc55af6
manish-dev3490 <kmanish87064@gmail.com>
2024-04-15 19:06:41 +0530

    doing body section

--- Todo-List/src/components/BodyCommponent.js
+++ Todo-List/src/components/BodyCommponent.js
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 // In this file i am making body component
 
 const BodyComponent=()=>{
+
+    const [TodoValue,setTodoValue]=useState("");
+    const [InputText,setInputText]=useState("");
+
     return (
         <>
         <div className="Body">
         <div className="todo-box">
-            <input type="text"></input>
-            <button className="add-todo">Add Todo</button>
+            <input type="text" onChange={(e)=>{
+                setInputText(e.target.value);
+            }}></input>
+            <button className="add-todo-btn" onClick={()=>{
+                
+                if(InputText===""){
+                    window.alert("Please enter your query to add in todo");
+                }
+
+                else if (InputText){
+                    setTodoValue(InputText);
+                }
+            }}>Add Todo</button>
+        </div>
+
+        <div className="todo-add-box">
+            <p className="list">{TodoValue}</p>
         </div>
         
         </div>
