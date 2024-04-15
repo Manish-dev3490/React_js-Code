58cea55c8402fddbd8ef1b24d30d74063391aa71
manish-dev3490 <kmanish87064@gmail.com>
2024-04-15 09:15:42 +0530

    Creating router for my application:

--- E-commerce/index.js
+++ E-commerce/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client"
 import { HeaderComponent } from "./src/components/HeaderComponent";
 import { HeroSection } from "./src/components/HeroSection";
 import { FooterLayout } from "./src/components/Footer";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter ,RouterProvider} from "react-router-dom";
+import { AllProductsPage } from "./src/components/AllProducts";
+import { ContactPage } from "./src/components/Contact";
+import { HelpPage } from "./src/components/Help";
+import { AboutPage } from "./src/components/About";
 
 
 
@@ -24,10 +28,25 @@ const ApplicationRouter = createBrowserRouter([
         element:<FullApplicationArea/>
     },
     {
-        path:"/About"
+        path:"/About",
+        element:<AboutPage/>
     }
+    ,
+    {
+        path:"/AllProduct",
+        element:<AllProductsPage/>
+    },
+    {
+        path:"/Contact",
+        element:<ContactPage/>
+    },
+    {
+        path:"/Help",
+        element:<HelpPage/>
+    }
+
 ])
 
 // This is the main root of the aplication where we have to render our all components
 const rootOfApplication=ReactDOM.createRoot(document.querySelector(".container"));
-rootOfApplication.render(<FullApplicationArea/>);
+rootOfApplication.render(<RouterProvider router={ApplicationRouter}/>);
