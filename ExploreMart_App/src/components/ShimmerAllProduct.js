9915e71aa0762d3e7a6c088ca9a3dda07354a657
manish-dev3490 <kmanish87064@gmail.com>
2024-04-17 11:05:12 +0530

    Shimmer ui

---
+++ ExploreMart_App/src/components/ShimmerAllProduct.js
@@ -0,0 +1,15 @@
+import ProductCard from "./ProductCard"
+// In this file i am  amking a Shimmer ui effect for All products which are on the All products nav section
+
+const ShimmerUiAllProducts = () => {
+    return (
+        <div className="all-product-list">
+
+
+            <ProductCard />
+
+        </div>
+    )
+}
+
+export default ShimmerUiAllProducts;
