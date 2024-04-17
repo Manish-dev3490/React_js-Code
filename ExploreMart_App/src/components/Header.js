3afc2348a47b7fb24af9302e535e2bf929e77137
manish-dev3490 <kmanish87064@gmail.com>
2024-04-17 10:48:51 +0530

    trying to implement  some feauturers

--- ExploreMart_App/src/components/Header.js
+++ ExploreMart_App/src/components/Header.js
@@ -2,33 +2,33 @@ import 'remixicon/fonts/remixicon.css'
 import { Link } from 'react-router-dom';
 
 // this is a header file 
-const HeaderComponent=()=>{
+const HeaderComponent = () => {
     return (
         <>
-        <div className="header">
-            <h2>Explore <span className='mart'>Mart</span></h2>
-            <div className="navigation-bar">
-                <ul>
-                   <Link to={"/"}> <li>Home</li></Link>
-                    <Link to={"/About"}><li>About</li></Link>
-                    <Link to={"/AllProduct"}><li>All Products</li></Link>
-                    <Link to={"/Contact"}><li>Contact</li></Link>
+            <div className="header">
+                <h2>Explore <span className='mart'>Mart</span></h2>
+                <div className="navigation-bar">
+                    <ul>
+                        <Link to={"/"}> <li className='active-bar active' >Home</li></Link>
+                        <Link to={"/About"}><li className='active-bar active'>About</li></Link>
+                        <Link to={"/AllProduct"}><li className='active-bar active'>All Products</li></Link>
+                        <Link to={"/Contact"}><li className='active-bar active'>Contact</li></Link>
 
-                </ul>
+                    </ul>
+                </div>
+
+                <div className="search-container">
+                    <input type="text"></input>
+                    <button className='srch-btn'>Search</button>
+                </div>
+
+                <div className="user-profile-info">
+                    <div className="user"><i className="ri-user-fill"></i></div>
+                    <button className="login-btn">Login</button>
+                    <div className="cart-section"><i className="ri-shopping-cart-2-line"></i></div>
+                </div>
+
             </div>
-
-            <div className="search-container">
-                <input type="text"></input>
-                <button className='srch-btn'>Search</button>
-            </div>
-
-        <div className="user-profile-info">
-            <div className="user"><i className="ri-user-fill"></i></div>
-            <button className="login-btn">Login</button>
-            <div className="cart-section"><i className="ri-shopping-cart-2-line"></i></div>
-        </div>
-
-        </div>
         </>
     )
 }
