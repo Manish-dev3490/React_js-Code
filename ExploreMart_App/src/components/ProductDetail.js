2d14ee48e131348b78415983cab1a7f08358615b
manish-dev3490 <kmanish87064@gmail.com>
2024-04-17 08:56:02 +0530

    implementing product detail functionality

---
+++ ExploreMart_App/src/components/ProductDetail.js
@@ -0,0 +1,9 @@
+// In this file i am going to implemenet the functionality of the Product Detail when user will click on product app willl bring user to this page which is product detail page
+
+const ProductDetail=()=>{
+    return (
+        <div className="product-detail-page">
+            
+        </div>
+    )
+}
