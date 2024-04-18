0b2ba23ef36ce9ecc315847973256dd2351d6ba9
manish-dev3490 <kmanish87064@gmail.com>
2024-04-18 10:18:07 +0530

    Learning useState in depth

---
+++ React-Interview-Stuff/src/components/Body.js
@@ -0,0 +1,12 @@
+ const Body=()=>{
+    return (
+        <>
+        <input type="text" placeholder="Enter first Number"></input>
+        <input type="text" placeholder="Enter second Number"></input>
+        <button>Add these numbers</button>
+        <h2>Result is {}</h2>
+        </>
+    )
+ }
+
+ export  default Body;
