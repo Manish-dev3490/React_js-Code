352a5c8993f0ee2111e2916815aa0f6b89e35822
manish-dev3490 <kmanish87064@gmail.com>
2024-04-18 16:24:25 +0530

    some learning

---
+++ Todo-List/practice.js
@@ -0,0 +1,33 @@
+// // learning stateful functions 
+
+// function add(n1,n2){
+   
+
+//     return ()=>{
+//         return  console.log(n1+n2);
+//     }
+
+// }
+
+// const fn=add(5,5);
+// fn();
+// fn();
+// fn();
+// fn();
+// fn();
+
+import React from "react";
+
+class Mycomponent extends React.Component{
+    constructor(props){
+
+        this.state={
+            count:0,
+            count:2
+        }
+    }
+
+    render(){
+        return <h2></h2>
+    }
+}
