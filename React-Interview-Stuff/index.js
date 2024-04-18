bafa8ec9b209e0ed6bcef300518a35aedbbbee39
manish-dev3490 <kmanish87064@gmail.com>
2024-04-18 10:18:32 +0530

    Learning some advance things of usestate hook

--- React-Interview-Stuff/index.js
+++ React-Interview-Stuff/index.js
@@ -1,1 +1,13 @@
 // in this project i am learning some important React hooks
+import Body from "./src/components/Body";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Application=()=>{
+    return(
+        <Body/>
+    )
+}
+
+const root=ReactDOM.createRoot(document.querySelector(".root"));
+root.render(<Application></Application>)
