cf004644648664225fb674efe36e500265007135
manish-dev3490 <kmanish87064@gmail.com>
2024-04-22 12:14:18 +0530

    i will set up it later

--- Accordion-feauture/index.js
+++ Accordion-feauture/index.js
@@ -1,2 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+import AccordionApp from "./src/components/Main";
+
+const root=ReactDOM.createRoot(document.querySelector(".root"));
+
+const Appdiv=()=>{
+return (
+    <div className="main">
+    <AccordionApp></AccordionApp>
+    </div>
+)
+}
+root.render(<Appdiv/>)
--- Accordion-feauture/src/utils/AccordionOBJ.js
+++ Accordion-feauture/src/utils/AccordionOBJ.js
@@ -1,9 +1,9 @@
 const AccordionList = [
   { id: 1, content: "I am accordion 1 content" },
-  { id: 1, content: "I am accordion 1 content" },
-  { id: 1, content: "I am accordion 1 content" },
-  { id: 1, content: "I am accordion 1 content" },
-  { id: 1, content: "I am accordion 1 content" },
+  { id: 2, content: "I am accordion 2 content" },
+  { id: 3, content: "I am accordion 3 content" },
+  { id: 4, content: "I am accordion 4 content" },
+  { id: 5, content: "I am accordion 5 content" },
 ];
 
 
