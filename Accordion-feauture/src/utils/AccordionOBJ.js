06f48a1acc8a7f0ef24d485098559585662933a3
manish-dev3490 <kmanish87064@gmail.com>
2024-04-22 11:51:13 +0530

    creating accordion for practice and making fundamentals clear

---
+++ Accordion-feauture/src/utils/AccordionOBJ.js
@@ -0,0 +1,10 @@
+const AccordionList = [
+  { id: 1, content: "I am accordion 1 content" },
+  { id: 1, content: "I am accordion 1 content" },
+  { id: 1, content: "I am accordion 1 content" },
+  { id: 1, content: "I am accordion 1 content" },
+  { id: 1, content: "I am accordion 1 content" },
+];
+
+
+export default AccordionList;
