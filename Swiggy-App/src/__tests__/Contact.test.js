2d63da9b2d165acd5c153640d9eb98b0c578d69b
manish-dev3490 <kmanish87064@gmail.com>
2024-04-26 14:30:44 +0530

    written unit test for contact page

--- Swiggy-App/src/__tests__/Contact.test.js
+++ Swiggy-App/src/__tests__/Contact.test.js
@@ -1,8 +1,16 @@
 import {render,screen} from "@testing-library/react"
 import { ContactPage } from "../components/Contact"
+import "@testing-library/jest-dom"
 
 test("should load contact page component ",()=>{
     render(<ContactPage/>)
     const heading=screen.getAllByRole("heading");
+    console.log(heading);
     expect(heading).toBeIntheDocument;
+})
+
+test("wehather it have input box load or not",()=>{
+    // querying
+    const inputBoxes=screen.findAllByRole("input")
+    expect(inputBoxes).toBeIntheDocument;
 })
