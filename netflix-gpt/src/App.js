6d867f322329bf7747a1dc7e0acde5bbd7c78b9c
manish-dev3490 <kmanish87064@gmail.com>
2024-04-27 12:22:18 +0530

    sign in form is doone

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div className="text-3xl text-red-400">
-      Namastey React Full Fledged Industry Level Project Application Netflix Gpt'
+    <div>
+      
     </div>
   );
 }
