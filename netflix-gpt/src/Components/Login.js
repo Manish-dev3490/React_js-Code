b200e7c3f98faaab2c97ee04ae7b04b6b2e573ac
manish-dev3490 <kmanish87064@gmail.com>
2024-04-27 13:43:36 +0530

    login /sign/sing up /header is  done

--- netflix-gpt/src/Components/Login.js
+++ netflix-gpt/src/Components/Login.js
@@ -1,6 +1,12 @@
+import { useState } from "react"
 import { Header } from "./Header"
 
 export const Login = () => {
+
+    const [isSignIn, setIsSignIn] = useState(true);
+    function handleSignInFuncationality() {
+        setIsSignIn(!isSignIn);
+    }
     return (
         <div >
             <Header />
@@ -8,12 +14,14 @@ export const Login = () => {
                 <img className="w-full bg-gradient-to-b from-black absolute" src="https://assets.nflxext.com/ffe/siteui/vlv3/c7f07b68-7989-4ff7-a31e-11c17dcc2fea/fcf685b8-3f9f-42d8-9af3-4bb86fa5a3b8/IN-en-20240422-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="background" />
             </div>
 
-            <form className=" bg-black w-4/12 relative mx-auto z-50 top-40 flex flex-col gap-5 px-6 rounded-lg bg-opacity-80 py-6">
-                <h2 className="text-white font-medium text-xl">Sign In</h2>
+            <form className=" h-auto bg-black w-4/12 relative mx-auto z-50 top-40 flex flex-col gap-5 px-6 rounded-lg bg-opacity-80 py-8">
+                <h2 className="text-white font-medium text-xl">{isSignIn ? "Sign In" : "Sign Up"}</h2>
+                {!isSignIn ? <input type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your full name" /> : ""}
                 <input type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your email" />
-                <input type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your password" />
-                <button className="bg-red-600 rounded-lg py-3 px-5 text-white text-xl">Sign In</button>
-                <p className="text-white cursor-pointer">New to Netflix ? <span> Sign Up Here</span></p>
+                <input type="password" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your password" />
+                {!isSignIn ? <input type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your mobile number" /> : ""}
+                <button className="bg-red-600 rounded-lg py-3 px-5 text-white text-xl">{isSignIn ? "Sign In" : "Sign Up"}</button>
+                <p className="text-white cursor-pointer" onClick={handleSignInFuncationality}>{!isSignIn ? "Already registered ? sign in" : "New to Netflix ? sign up here"}</p>
             </form>
 
         </div>
