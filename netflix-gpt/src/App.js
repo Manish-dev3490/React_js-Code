9e6e5b423af0b645f99a6e06ff900528a0ecdc61
manish-dev3490 <kmanish87064@gmail.com>
2024-04-27 09:43:02 +0530

    cra && tailwind is setuped

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <h2 className="text-3xl text-green-400 ">Namastey React Full Fledged Industry Level Project Application Netflix Gpt's</h2>
+    </div>
+  );
+}
+
+export default App;
