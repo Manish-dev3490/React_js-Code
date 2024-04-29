2cdda9301cbc19820cb72b39f7303e11a2a68476
manish-dev3490 <kmanish87064@gmail.com>
2024-04-29 14:31:06 +0530

    sign/in/up/authentication using firebase done

--- netflix-gpt/src/Components/Login.js
+++ netflix-gpt/src/Components/Login.js
@@ -1,11 +1,14 @@
-import { useState, useRef } from "react"
-import { CheckValidation } from "../utils/validate"
-import { Header } from "./Header"
+import { useState, useRef } from "react";
+import { CheckValidation } from "../utils/validate";
+import { Header } from "./Header";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/Firebase";
 export const Login = () => {
-
     const [isSignIn, setIsSignIn] = useState(true);
     const [errorMessage, seterrorMessage] = useState(null);
-
 
     function handleSignInFuncationality() {
         setIsSignIn(!isSignIn);
@@ -13,46 +16,117 @@ export const Login = () => {
 
     let email = useRef(null);
     let password = useRef(null);
-   
 
 
     function handleSignButton() {
-        const message = CheckValidation(email.current.value, password.current.value);
-        if(email.current.value==="" || password.current.value===""){
-            alert("Please enter your data in input fields")
+        const message = CheckValidation(
+            email.current.value,
+            password.current.value
+        );
+        seterrorMessage(message);
+
+        if (email.current.value === "" || password.current.value === "") {
+            alert("Please enter your data in input fields");
         }
 
+        if (message) return;
 
-        seterrorMessage(message);
-    
+        if (!isSignIn) {
+            // config for sign up
 
+            
+            createUserWithEmailAndPassword(
+                auth,
+                email.current.value,
+                password.current.value
+            )
+                .then((userCredential) => {
+                    // Signed up
+                    const user = userCredential.user;
+                    console.log(user);
+                    // ...
+                })
+                .catch((error) => {
+                   
+                    seterrorMessage("The user have already signed in to the account");
+                    // ..
+                });
+        } else {
+            // config for sign in
+
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log(user);
+                    // ...
+                })
+                .catch((error) => {
+                    
+                    seterrorMessage("did not found this user in our backend")
+                });
+        }
     }
 
-
-
-
     return (
-        <div >
+        <div>
             <Header />
-            <div >
-                <img className="w-full bg-gradient-to-b from-black absolute" src="https://assets.nflxext.com/ffe/siteui/vlv3/c7f07b68-7989-4ff7-a31e-11c17dcc2fea/fcf685b8-3f9f-42d8-9af3-4bb86fa5a3b8/IN-en-20240422-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="background" />
+            <div>
+                <img
+                    className="w-full bg-gradient-to-b from-black absolute"
+                    src="https://assets.nflxext.com/ffe/siteui/vlv3/c7f07b68-7989-4ff7-a31e-11c17dcc2fea/fcf685b8-3f9f-42d8-9af3-4bb86fa5a3b8/IN-en-20240422-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+                    alt="background"
+                />
             </div>
 
-            <form onSubmit={(e) => e.preventDefault()} className=" h-auto bg-black w-4/12 relative mx-auto z-50 top-40 flex flex-col gap-5 px-6 rounded-lg bg-opacity-80 py-8">
-                <h2 className="text-white font-medium text-xl">{isSignIn ? "Sign In" : "Sign Up"}</h2>
-                <input ref={email} type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your email" />
-                <input ref={password} type="password" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your password" />
-                {!isSignIn ? <input  type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your full name" /> : ""}
+            <form
+                onSubmit={(e) => e.preventDefault()}
+                className=" h-auto bg-black w-4/12 relative mx-auto z-50 top-40 flex flex-col gap-5 px-6 rounded-lg bg-opacity-80 py-8"
+            >
+                <h2 className="text-white font-medium text-xl">
+                    {isSignIn ? "Sign In" : "Sign Up"}
+                </h2>
+                <input
+                    ref={email}
+                    type="text"
+                    className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 "
+                    placeholder="Enter your email"
+                />
+                <input
+                    ref={password}
+                    type="password"
+                    className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 "
+                    placeholder="Enter your password"
+                />
+                {!isSignIn ? (
+                    <input
+                        type="text"
+                        className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 "
+                        placeholder="Enter your full name"
+                    />
+                ) : (
+                    ""
+                )}
 
-
-                {!isSignIn ? <input type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your mobile number" /> : ""}
+                {/* 
+                {!isSignIn ? <input type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your mobile number" /> : ""} */}
                 <p className="text-red-600">{errorMessage}</p>
 
-
-                <button onClick={handleSignButton} className="bg-red-600 rounded-lg py-3 px-5 text-white text-xl">{isSignIn ? "Sign In" : "Sign Up"}</button>
-                <p className="text-white cursor-pointer" onClick={handleSignInFuncationality}>{!isSignIn ? "Already registered ? sign in" : "New to Netflix ? sign up here"}</p>
+                <button
+                    onClick={handleSignButton}
+                    className="bg-red-600 rounded-lg py-3 px-5 text-white text-xl"
+                >
+                    {isSignIn ? "Sign In" : "Sign Up"}
+                </button>
+                <p
+                    className="text-white cursor-pointer"
+                    onClick={handleSignInFuncationality}
+                >
+                    {!isSignIn
+                        ? "Already registered ? sign in now "
+                        : "New to Netflix ? sign up here"}
+                </p>
             </form>
-
         </div>
-    )
-}
+    );
+};
--- netflix-gpt/src/utils/validate.js
+++ netflix-gpt/src/utils/validate.js
@@ -1,24 +1,27 @@
 // this  is used for form validation 
- export const CheckValidation=(email,password)=>{
-    const emailRegex=/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-    const passwordRegex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/.test(password);
+export const CheckValidation = (email, password) => {
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/.test(password);
 
 
-  
 
-   
-   
-    if(email && password){
-        if(!emailRegex) return "your email is not valid";
-        if(!passwordRegex) return "your password is not valid";
+
+
+
+    if (email && password) {
+        if (!emailRegex) return "your email is not valid";
+        if (!passwordRegex) return "your password is not valid";
     }
 
-  
+
 
 
     return null;
 
 }
+
+
+
 
 
 
