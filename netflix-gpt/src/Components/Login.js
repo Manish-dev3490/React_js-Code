41b91f5322f1998da3b2fe8aff7acb97637ccce8
manish-dev3490 <kmanish87064@gmail.com>
2024-04-29 11:20:01 +0530

    Deployed on firebase validation is  done

--- netflix-gpt/src/Components/Login.js
+++ netflix-gpt/src/Components/Login.js
@@ -1,21 +1,35 @@
 import { useState, useRef } from "react"
-import CheckValidation from "../utils/validate"
+import { CheckValidation } from "../utils/validate"
 import { Header } from "./Header"
 export const Login = () => {
 
     const [isSignIn, setIsSignIn] = useState(true);
-    const [errorMessage,seterrorMessage]=useState(null);
+    const [errorMessage, seterrorMessage] = useState(null);
+
+
     function handleSignInFuncationality() {
         setIsSignIn(!isSignIn);
     }
 
     let email = useRef(null);
     let password = useRef(null);
+   
+
 
     function handleSignButton() {
-        const message = CheckValidation(email.current.value, password.current.value)
+        const message = CheckValidation(email.current.value, password.current.value);
+        if(email.current.value==="" || password.current.value===""){
+            alert("Please enter your data in input fields")
+        }
+
+
         seterrorMessage(message);
+    
+
     }
+
+
+
 
     return (
         <div >
@@ -26,11 +40,15 @@ export const Login = () => {
 
             <form onSubmit={(e) => e.preventDefault()} className=" h-auto bg-black w-4/12 relative mx-auto z-50 top-40 flex flex-col gap-5 px-6 rounded-lg bg-opacity-80 py-8">
                 <h2 className="text-white font-medium text-xl">{isSignIn ? "Sign In" : "Sign Up"}</h2>
-                {!isSignIn ? <input type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your full name" /> : ""}
                 <input ref={email} type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your email" />
                 <input ref={password} type="password" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your password" />
+                {!isSignIn ? <input  type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your full name" /> : ""}
+
+
                 {!isSignIn ? <input type="text" className="text-white bg-gray-700 rounded-lg py-3 bg-opacity-80 px-5 " placeholder="Enter your mobile number" /> : ""}
                 <p className="text-red-600">{errorMessage}</p>
+
+
                 <button onClick={handleSignButton} className="bg-red-600 rounded-lg py-3 px-5 text-white text-xl">{isSignIn ? "Sign In" : "Sign Up"}</button>
                 <p className="text-white cursor-pointer" onClick={handleSignInFuncationality}>{!isSignIn ? "Already registered ? sign in" : "New to Netflix ? sign up here"}</p>
             </form>
--- netflix-gpt/src/utils/validate.js
+++ netflix-gpt/src/utils/validate.js
@@ -1,12 +1,26 @@
 // this  is used for form validation 
-const CheckValidation=(email,password)=>{
+ export const CheckValidation=(email,password)=>{
     const emailRegex=/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
     const passwordRegex=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/.test(password);
 
-    if(!emailRegex) return "your email is not valid";
-    if(!passwordRegex) return "your password is not valid";
+
+  
+
+   
+   
+    if(email && password){
+        if(!emailRegex) return "your email is not valid";
+        if(!passwordRegex) return "your password is not valid";
+    }
+
+  
+
 
     return null;
 
 }
-export default CheckValidation;
+
+
+
+
+
