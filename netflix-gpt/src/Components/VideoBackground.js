b5891094af1027ed7b6f41ab57f956a2862a6bdb
manish-dev3490 <kmanish87064@gmail.com>
2024-05-06 17:54:55 +0530

    video background component

--- netflix-gpt/src/Components/VideoBackground.js
+++ netflix-gpt/src/Components/VideoBackground.js
@@ -2,14 +2,16 @@ import { useSelector } from "react-redux";
 import useMovieTrailor from "../hooks/useMovieTrailor";
 
 const Videobackground = ({ id }) => {
+
     const ShowTrailor = useSelector((store) => store.movies.movietrailor);
     useMovieTrailor(id);
 
 
+
     return (
-        <>
-            <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + ShowTrailor?.key} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-        </>
+    
+            <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + ShowTrailor?.key} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        
     )
 
 };
