cca3233154452328d73b55b9b76407ad6656a09a
manish-dev3490 <kmanish87064@gmail.com>
2024-05-14 10:21:10 +0530

    we are done with our application

--- netflix-gpt/src/Components/MovieCard.js
+++ netflix-gpt/src/Components/MovieCard.js
@@ -4,7 +4,7 @@ export const MovieCard=({poster_path})=>{
     if(!poster_path)return null
 
     return (
-    
+        
         <div className="w-48 pr-4 cursor-pointer ">
          <img alt="pic" src={IMG_CDN_URL+poster_path} className="rounded-lg h-64 w-48" />
     </div>
