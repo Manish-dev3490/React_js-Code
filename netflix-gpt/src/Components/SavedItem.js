4d1d069b87dc28a199767ccfdb327c7f353c5fe6
manish-dev3490 <kmanish87064@gmail.com>
2024-05-14 09:29:13 +0530

    some styling issue

--- netflix-gpt/src/Components/SavedItem.js
+++ netflix-gpt/src/Components/SavedItem.js
@@ -1,21 +1,21 @@
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { IMG_CDN_URL } from "../utils/constant"
 import { deleteSavedMovie } from "../utils/SavedMovieSlice";
 
 export const SavedItem = ({ tagline, overview, title, backdrop_path }) => {
-const dispatch=useDispatch();
-    
+    const dispatch = useDispatch();
 
-    const DeleteMovie=()=>{
+
+    const DeleteMovie = () => {
         dispatch(deleteSavedMovie())
     }
     return (
-        <div className="w-screen border-b border-black flex gap-5 py-2 px-4 items-center ">
-            <img src={IMG_CDN_URL + backdrop_path} alt="pic" className="  w-32  h h-52      mt-16 rounded-sm" />
+        <div className="w-screen border-b border-black flex gap-5 items-center pb-4 px-4 ">
+            <img src={IMG_CDN_URL + backdrop_path} alt="pic" className="  w-40   h-60       mt-16 rounded-lg" />
 
-            <div className="flex flex-col gap-2 mt-2">
+            <div className="flex flex-col gap-4  ">
                 <h2 className="text-lg font-medium ">{title}</h2>
-                <p className="">{overview}</p>
+                <p className="text-sm">{overview}</p>
                 <p className="">{tagline}</p>
 
                 <button onClick={DeleteMovie} className=" bg-red-500  w-32 py-1 rounded-lg  ">Delete Movie</button>
--- netflix-gpt/src/Components/SavedMovieList.js
+++ netflix-gpt/src/Components/SavedMovieList.js
@@ -13,7 +13,7 @@ return(
        {savedMovies.length&&<div className="flex flex-col">{savedMovies.map((movie)=><SavedItem tagline={movie?.tagline} title={movie?.title} overview={movie?.overview} backdrop_path={movie?.backdrop_path}/>)}</div>}
        {!savedMovies.length?<div className=" mt-20 items-center w-screen h-[100%] flex flex-col gap-16">
          <h2 className="text-3xl">Your Saved Movie List <span className="text-red-500">is Empty</span></h2>
-         <img className="w-40  h-40" src={EMPTY_CartIMG} alt="Empty page"/>
+         <img className="w-40    rounded-sm h-40" src={EMPTY_CartIMG} alt="Empty page"/>
          <p>Add Some Movies to see Them In The Saved Movie List</p>
          <Link to={"/browse"}> <button className="bg-black py-2 rounded-md text-red-500 px-5">Return To Browse Page</button></Link>
          </div>:""
