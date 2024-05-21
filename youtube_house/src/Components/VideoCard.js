b0f093b8b6536aede338cc479d7160401540e654
manish-dev3490 <kmanish87064@gmail.com>
2024-05-21 18:02:30 +0530

    Homepage is done

--- youtube_house/src/Components/VideoCard.js
+++ youtube_house/src/Components/VideoCard.js
@@ -3,10 +3,10 @@ import React from 'react'
 const VideoCard = ({data}) => {
     
     return (
-        data && <div className=' w-64 shadow-lg  h-auto'>
+        data && <div className=' w-64 shadow-lg  h-auto px-2 py-2'>
             <img src={data?.snippet?.thumbnails?.medium?.url} className='rounded-md' alt="Video-card" />
-            <p className='truncate'>{data?.snippet?.description}</p>
-            <span className='flex gap-4'>{data?.statistics?.viewCount} views<span>{data?.snippet?.channelTitle
+            <p className='font-medium truncate'>{data?.snippet?.title}</p>
+            <span className='flex gap-4 text-sm'>{data?.statistics?.viewCount} views<span>{data?.snippet?.channelTitle
             }</span></span>
         </div>
     )
