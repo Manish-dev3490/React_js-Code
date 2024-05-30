c6654d9cdd2b47108f11dde2f8a2d905f0cca6d4
manish-dev3490 <kmanish87064@gmail.com>
2024-05-30 16:51:14 +0530

    styling fix

--- youtube_house/src/Components/Comments.js
+++ youtube_house/src/Components/Comments.js
@@ -4,17 +4,17 @@ import { useSelector } from 'react-redux';
 
 const Comments = () => {
   const commentsList=useSelector((store)=>store?.Videos?.comments)
-  console.log(commentsList);
   useComments();
   return (
-    <div className=' px-8 -mt-20 w-5/12'>
+    <div className='  px-8 -mt-20 w-9/12'>
       Comments : {commentsList.length}
-      {
+      {commentsList&&
         commentsList.map((commentData)=>{
-          return <div key={commentData.id} className=' mt-3 flex  gap-20'>
+          return <div key={commentData.id} className=' rounnded-xl bg-gray-100 py-3 px-3 mt-3 flex gap-10'>
             <img className=' w-8 h-8  rounded-full' src={commentData?.snippet?.topLevelComment?.snippet?.authorProfileImageUrl
             ?commentData?.snippet?.topLevelComment?.snippet?.authorProfileImageUrl:""} alt=''/>
             <div>
+              <p className='font-medium'>{commentData?.snippet?.topLevelComment?.snippet?.authorDisplayName}</p>
               <p>{commentData?.snippet?.topLevelComment?.snippet?.textOriginal}</p>
             </div>
           </div>
